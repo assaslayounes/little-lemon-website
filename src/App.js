b3290106fde5c7cd65a82f24da6fd0b697d3ce1c
Younes ASSASLA <assaslayounes@yahoo.fr>
2025-01-10 21:58:41 +0100

    Adding semantic structure, meta tags and setting up OGP

--- src/App.js
+++ src/App.js
@@ -3,9 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Little lemon website</h1>
-    </div>
+    <>
+      <header>
+        <h2>Little Lemon</h2>
+      </header>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Menu</a></li>
+          <li><a href="/">Reservations</a></li>
+          <li><a href="/">Order Online</a></li>
+          <li><a href="/">Login</a></li>
+        </ul>
+      </nav>/
+      <main>
+        <section>
+          <h1>Little Lemon</h1>
+          <p>Chicago's We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <button>Reserve a Table</button>
+        </section>
+        <section>
+          <h2>Specials</h2>
+          <ul>
+            <li>Greek Salad</li>
+            <li>Bruschetta</li>
+            <li>Lemon Dessert</li>
+          </ul>
+        </section>
+      </main>
+      <footer>
+        <p>© 2023 Little Lemon. All rights reserved.</p>
+      </footer>
+    </>
   );
 }
 
